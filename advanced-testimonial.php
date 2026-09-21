b7905b486bb72e2a9671e3014d8d4a24933d9973
devmonowar <devmonowar@gmail.com>
2026-09-21 12:24:18 +0600

    Plugin URI points at site landing page

--- advanced-testimonial.php
+++ advanced-testimonial.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       Advanced Testimonial
- * Plugin URI:        https://wordpress.org/plugins/advanced-testimonial/
+ * Plugin URI:        https://devmonowar.github.io/advanced-testimonial/
  * Description:       Showcase customer testimonials and reviews in beautiful grids, carousels, cards and more. Lightweight, block-ready and built for social proof.
  * Version:           2.0.9
  * Requires at least: 6.5
